fa3f89d8c8737a932e3701a95e1acfd4c1c6a20f
Dylan Bartley <dbartley@clearharbor.com>
2023-11-24 13:24:45 -0500

    added proper region extraction

--- SaiphIamRolesAnywhereTests/StringTests.cs
+++ SaiphIamRolesAnywhereTests/StringTests.cs
@@ -22,5 +22,17 @@ namespace SaiphIamRolesAnywhereTests
             var expected = "profileArn=arn%3Aaws%3Arolesanywhere%3Aus-east-1%3A12345678%3Aprofile%2Fe6a5cf90-8983-4f32-bacf-75614d7e3343&roleArn=arn%3Aaws%3Aiam%3A%3A12345678%3Arole%2Fsomerole&trustAnchorArn=arn%3Aaws%3Arolesanywhere%3Aus-east-1%3A12345678%3Atrust-anchor%2Fe6a5cf90-fc93-478a-bacf-75614d7e3343";
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData("arn:aws:rolesanywhere:us-east-1:12345678:trust-anchor/e6a5cf90-fc93-478a-bacf-75614d7e3343", "us-east-1")]
+        [InlineData("us-west-2-bacf-75614d7e3343", "us-west-2")]
+        [InlineData("arn:::us-isob-east-1:12345678/75614d7e3343", "us-isob-east-1")]
+        [InlineData("no-valid region in string", "")]
+        [InlineData(null, "")]
+        public void ExtractRegionFromArn(string inputArn, string expectedRegion)
+        {
+            var result = Utility.ExtractRegion(inputArn);
+            Assert.Equal(expectedRegion, result);
+        }
     }
 }
