0ecc8472032e159e8931220d849b6b7984406862
Michael Moorman <michael.moorman@drakesoftware.com>
2024-08-15 17:25:08 -0400

    add passwod finder to service
    This modifications makes password finder available to clients of the RolesAnywhereService class. The default password is null.

--- SaiphIamRolesAnywhere/CertificateProvider.cs
+++ SaiphIamRolesAnywhere/CertificateProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
 using Org.BouncyCastle.Security;
+using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
@@ -8,66 +9,40 @@ using System.Text;
 
 namespace SaiphIamRolesAnywhere
 {
-    public static class CertificateProvider
-    {
-        //public static (RSA, X509Certificate) ImportWindowsStore(string subject, StoreLocation storeLocation = StoreLocation.LocalMachine)
-        //{
-        //    foreach (StoreName storeName in (StoreName[])Enum.GetValues(typeof(StoreName)))
-        //    {
-        //        X509Store store = new X509Store(storeName, storeLocation);
-        //        store.Open(OpenFlags.ReadOnly);
+	public static class CertificateProvider
+	{
+		public static IPasswordFinder NoPassword = new NullPassword();
+		public static (RSA, X509Certificate) ImportPfx(string certificatePath) => ImportPfx(certificatePath, NoPassword);
+		public static (RSA, X509Certificate) ImportPfx(string certificatePath, IPasswordFinder finder)
+		{
+			X509Certificate2Collection collection = new X509Certificate2Collection();
+			collection.Import(certificatePath, new string(finder.GetPassword()), X509KeyStorageFlags.PersistKeySet);
 
-        //        Console.WriteLine("Yes    {0,4}  {1}, {2}",
-        //            store.Certificates.Count, store.Name, store.Location);
+			var cert = collection[0];
+			var rsa = cert.GetRSAPrivateKey();
 
-        //        var collection = store.Certificates.Find(X509FindType.FindBySubjectName, subject, true);
-        //        if (collection.Count > 0)
-        //        {
-        //            var cert = collection[0];
+			return (rsa, cert);
+		}
 
-        //            //var certPem = cert.GetBase64String();
-        //            //var pemReader = new PemReader(new StringReader(certPem));
-        //            var key = cert.GetPublicKeyString();
-        //            var rsa = cert.GetRSAPublicKey();
-        //            Console.WriteLine(rsa.ToString());
-        //            //var key = pemReader.read
-        //            //var rsaParams = DotNetUtilities.ToRSAParameters(key);
+		public static (RSA, X509Certificate) FromPemFiles(string privateKeyPath, string certificatePath, IPasswordFinder finder)
+		{
+			var cert = X509Certificate.CreateFromCertFile(certificatePath);
 
-        //            //var rsa = RSA.Create();
-        //            //rsa.ImportParameters(rsaParams);
+			var keypem = Encoding.UTF8.GetString(File.ReadAllBytes(privateKeyPath));
+			var pemReader = new PemReader(new StringReader(keypem), finder);
 
-        //            return (null, cert);
-        //        }
-        //    }
+			var key = (RsaPrivateCrtKeyParameters)pemReader.ReadObject();
+			var rsaParams = DotNetUtilities.ToRSAParameters(key);
 
-        //    throw new Exception($"Certificate {subject}");
-        //}
+			var rsa = RSA.Create();
+			rsa.ImportParameters(rsaParams);
 
-        public static (RSA, X509Certificate) ImportPfx(string certificatePath)
-        {
-            X509Certificate2Collection collection = new X509Certificate2Collection();
-            collection.Import(certificatePath, null, X509KeyStorageFlags.PersistKeySet);
+			return (rsa, cert);
+		}
+	}
+	public class NullPassword : IPasswordFinder
+	{
+		public char[] GetPassword() => null;
+	}
 
-            var cert = collection[0];
-            var rsa = cert.GetRSAPrivateKey();
-
-            return (rsa, cert);
-        }
-
-        public static (RSA, X509Certificate) FromPemFiles(string privateKeyPath, string certificatePath)
-        {       
-            var cert = X509Certificate.CreateFromCertFile(certificatePath);
-
-            var keypem = Encoding.UTF8.GetString(File.ReadAllBytes(privateKeyPath));
-            var pemReader = new PemReader(new StringReader(keypem));
-
-            var key = (RsaPrivateCrtKeyParameters)pemReader.ReadObject();
-            var rsaParams = DotNetUtilities.ToRSAParameters(key);
-
-            var rsa = RSA.Create();
-            rsa.ImportParameters(rsaParams);
-
-            return (rsa, cert);
-        }
-    }
 }
